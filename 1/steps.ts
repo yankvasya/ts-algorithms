ba82772c05a63290e28d6bf5e471aab79a7e298d
yankvasya <yankvasya@gmail.com>
2022-07-01 02:20:09 +0300

    fix: changed structure for 1/steps.ts

--- 1/steps.ts
+++ 1/steps.ts
@@ -1,4 +1,11 @@
-const steps = (num, count) =>
-  num === 1 ? count || 1 : steps(Math.round(num / 2), ++count);
+let count = 0;
 
-export default steps;
+const steps = (num) =>
+  num === 1 ? count || 1 : ++count && steps(Math.round(num / 2));
+
+const findSteps = (num) => {
+  count = num === 2 ? 1 : 0;
+  return steps(num);
+};
+
+export default findSteps;
--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-import steps from "./1/steps";
+import findSteps from "./1/steps";
 
 const type = process.argv.slice(2)[0];
 
 switch (type) {
   case "steps":
     const number = Number(process.argv.slice(2)[1]);
-    const count = number === 2 ? 1 : 0;
-    const result = steps(number, count);
+    const result = findSteps(number);
 
     console.log("Steps for binary search: ", result);
     break;
